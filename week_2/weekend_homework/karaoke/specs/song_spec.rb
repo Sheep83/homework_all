44d0048b9be3b35f0edc6bb850fd8af641c55cb7
Sheep83 <bmorrice83@googlemail.com>
2016-06-17 22:57:06 +0100

    songs and guests working

--- week_2/weekend_homework/karaoke/specs/song_spec.rb
+++ week_2/weekend_homework/karaoke/specs/song_spec.rb
@@ -1,7 +1,6 @@
 require 'pry-byebug'
 require ('minitest/autorun')
 require_relative ('../bar')
-require_relative ('../song')
 require_relative ('../room')
 require_relative ('../guest')
 
@@ -9,8 +8,19 @@ require_relative ('../guest')
 class TestSong < Minitest::Test
 
   def setup
-     
-  end
+      song1 = Song.new("Pink Floyd", "Comfortably Numb")
+      song2 = Song.new("Rolling Stones", "Gimme Shelter")
+      song3 = Song.new("Foo Fighters", "Everlong")
+      song4 = Song.new("Led Zeppelin", "The Song Remains The Same") 
+      @songs = [song1, song2, song3, song4] 
+      # binding.pry
+    end
+
+    def test_song_artist_and_title_from_array
+    assert_equal( "Pink Floyd", @songs[0].artist )
+    assert_equal( "Comfortably Numb", @songs[0].title )
+    end
+
 
 end
 
