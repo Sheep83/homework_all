099b8faef75ea7cc44e181c0a060e4b6d1112730
Sheep83 <bmorrice83@googlemail.com>
2016-06-17 10:45:12 +0100

    initial commit

---
+++ week_2/weekend_homework/karaoke/structure.rb
@@ -0,0 +1,22 @@
+#classes required
+
+#ROOMS
+#must be able to accept guests and songs
+#initialize as empty? or pass #songs and #guests as parameters?
+
+#SONGS
+#must be able to be be passed to rooms
+#initialize with artist name and title - randomly selected from hash of titles
+
+
+#GUESTS
+#must be able to be passed to rooms
+#initialize with name and cash balance
+#optional - text string when in room with favourite song
+
+#BAR
+#class to contain all other classes
+#methods to add guests and songs to rooms as well as charge customers
+
+
+
---
+++ week_2/weekend_homework/structure.rb
@@ -0,0 +1,11 @@
+#classes required
+
+#ROOMS
+#must be able to accept guests and songs
+
+#SONGS
+#must be able to be be passed to rooms
+
+#GUESTS
+#must be able to be passed to rooms
+
