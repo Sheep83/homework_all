1eb788d5fd582241561a99eee48ce94398bfe9e8
Sheep83 <bmorrice83@googlemail.com>
2016-07-02 18:55:22 +0100

    basic db actions added

--- week_5/Moneycashboard/db/seeds.rb
+++ week_5/Moneycashboard/db/seeds.rb
@@ -1,4 +1,5 @@
 require('pg')
+require('pry-byebug')
 require_relative('sql_runner')
 require_relative('../models/account')
 require_relative('../models/merchant')
@@ -32,42 +33,84 @@ transaction1_init = {
   'merchant_id' => '1',
   'tag_id' => '1',
   'amount' => '25',
-  'trans_date' => '2016-07-01',
-  'description' => 'desc1',
-  'type' => 'cash'
+  'trans_date' => "'2016-07-01'",
+  'description' => "'blah1'",
+  'type' => "'cash'"
 }
 transaction2_init = {
   'account_id' => '1',
   'merchant_id' => '2',
   'tag_id' => '1',
   'amount' => '50',
-  'trans_date' => '2016-07-02',
-  'description' => 'desc2',
-  'type' => 'debit'
+  'trans_date' => "'2016-07-02'",
+  'description' => "'blah2'",
+  'type' => "'debit'"
 }
 transaction3_init = {
   'account_id' => '1',
   'merchant_id' => '1',
   'tag_id' => '2',
   'amount' => '75',
-  'trans_date' => '2016-06-30',
-  'description' => 'desc3',
-  'type' => 'cash'
+  'trans_date' => "'2016-06-30'",
+  'description' => "'blah3'",
+  'type' => "'cash'"
 }
 transaction4_init = {
-  'account_id' => '1',
+  'account_id' => '2',
   'merchant_id' => '1',
   'tag_id' => '2',
   'amount' => '100',
-  'trans_date' => '2016-07-02',
-  'description' => 'desc4',
-  'type' => 'debit'
+  'trans_date' => "'2016-07-02'",
+  'description' => "'blah4'",
+  'type' => "'debit'"
+}
+trans_update = {
+  'id' => '1',
+  'amount' => '500'
+}
+
+merch_update = {
+  'name' => 'overclockers', 
+  'id' => '1'
 }
 
 account1 = Account.new( account1_init )
 account2 = Account.new( account2_init )
 account1.save
 account2.save
+merchant1 = Merchant.new( merchant1_init )
+merchant2 = Merchant.new( merchant2_init )
+merchant1.save
+merchant2.save
+tag1 = Tag.new( tag1_init )
+tag2 = Tag.new( tag2_init )
+tag1.save
+tag2.save
+
+
+
+trans1 = Transaction.new( transaction1_init )
+trans2 = Transaction.new( transaction2_init )
+trans3 = Transaction.new( transaction3_init )
+trans4 = Transaction.new( transaction4_init )
+trans1.save
+trans2.save
+trans3.save
+trans4.save
+
+binding.pry
+
+# Transaction.by_tag(1)
+
+# Merchant.update(merch_update)
+
+# Transaction.delete(2)
+
+# Transaction.find(1)
+
+nil
+
+
 
 
 
