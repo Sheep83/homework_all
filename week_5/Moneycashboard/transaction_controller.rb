3a86ec3335c361e21ecc37bd5fde059f34e31608
Sheep83 <bmorrice83@googlemail.com>
2016-07-13 00:32:13 +0100

    final commit

--- week_5/Moneycashboard/transaction_controller.rb
+++ week_5/Moneycashboard/transaction_controller.rb
@@ -8,10 +8,10 @@ require_relative ('./models/tag')
 
 
 get '/transaction/new' do
-@accounts = Account.all
-@tags = Tag.all
-@merchants = Merchant.all
-erb ( :'transactions/new' )
+  @accounts = Account.all
+  @tags = Tag.all
+  @merchants = Merchant.all
+  erb ( :'transactions/new' )
 end
 
 get '/transaction' do
@@ -22,12 +22,12 @@ get '/transaction' do
 end
 
 post '/transaction' do
-@transaction = Transaction.new(params)
-@account = Account.find(@transaction.account_id)
-@account.balance -= @transaction.amount
-@transaction.save()
-@account.update()
-erb ( :'transactions/create' )
+  @transaction = Transaction.new(params)
+  @account = Account.find(@transaction.account_id)
+  @account.balance -= @transaction.amount
+  @transaction.save()
+  @account.update()
+  erb ( :'transactions/create' )
 end
 
 get '/transaction/:id' do 
@@ -36,8 +36,8 @@ get '/transaction/:id' do
 end
 
 get'/transaction/:id/edit' do
-@transaction = Transaction.find(params[:id])
-erb (:'transactions/edit')
+  @transaction = Transaction.find(params[:id])
+  erb (:'transactions/edit')
 end
 
 post'/transaction/:id' do 
