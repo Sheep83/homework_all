b0cdbfadcb1073ac8663e355873a281246df4bb0
Sheep83 <bmorrice83@googlemail.com>
2016-07-16 08:01:55 +0100

    customer can sell record

--- week_6/weekend/record_store/customer.js
+++ week_6/weekend/record_store/customer.js
@@ -7,11 +7,25 @@ var Customer = function(name, cash, inventory){
 Customer.prototype = {
   buyRecord: function(title, store){
     store.inventory.forEach(function(record){
+      var index = store.inventory.indexOf(record);
       if(title === record.title){
         var bought = record;
         this.inventory.push(bought);
+        store.inventory.splice(index, 1);
         this.cash -= bought.price;
         store.cash += bought.price;
+      }
+    }.bind(this))    
+  }, 
+  sellRecord: function(title, store){
+    this.inventory.forEach(function(record){
+      var index = this.inventory.indexOf(record);
+      if(title === record.title){
+        var sold = record;
+        store.inventory.push(sold);
+        this.inventory.splice(index, 1);
+        this.cash += sold.price;
+        store.cash -= sold.price;
       }
     }.bind(this))    
   }
--- week_6/weekend/record_store/specs/customer_spec.js
+++ week_6/weekend/record_store/specs/customer_spec.js
@@ -37,6 +37,28 @@ describe('Customer', function(){
     assert.equal( 508.99, store1.cash ); 
     assert.equal( "Doolittle", customer1.inventory[0].title );
     assert.equal( 91.01, customer1.cash );
+    assert.equal( 2, store1.inventory.length );
+    assert.equal( 1, customer1.inventory.length );
+  }),
+  it("can sell a record", function(){
+    var store1 = new Store( "Stone Grooves", "Aberdeen", [], 500 );
+    var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99);
+    var record2 = new Record ( "Stone Roses", "Stone Roses", 5.99);
+    var record3 = new Record ( "Pixies", "Doolittle", 8.99);
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+    var customer1 = new Customer( "Brian", 100, [] );
+    customer1.buyRecord( "Doolittle", store1);
+    customer1.buyRecord( "Stone Roses", store1);
+    customer1.buyRecord( "Ride the Lightning", store1);
+    assert.equal( 519.97, store1.cash ); 
+    assert.equal( 3, customer1.inventory.length );
+    assert.equal( 0, store1.inventory.length );
+    customer1.sellRecord( "Stone Roses", store1);
+    assert.equal( 2, customer1.inventory.length );
+    assert.equal( 1, store1.inventory.length );
+    assert.equal( 513.98, store1.cash ); 
   })
 })
 
