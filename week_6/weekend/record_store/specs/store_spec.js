98fa428c8fe994c42f7c320e1d9d0f634b5939a4
Sheep83 <bmorrice83@googlemail.com>
2016-07-16 05:38:18 +0100

    can sell record

--- week_6/weekend/record_store/specs/store_spec.js
+++ week_6/weekend/record_store/specs/store_spec.js
@@ -25,5 +25,23 @@ describe('Store', function(){
     var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99);
     store1.addRecord(record1);
     assert.equal( "Metallica", store1.inventory[0].artist )
+  }),
+  it("can list inventory", function(){
+    var store1 = new Store( "Stone Grooves", "Aberdeen", [], 500 );
+    var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99);
+    var record2 = new Record ( "Stone Roses", "Stone Roses", 5.99);
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.listInventory();
+    assert.equal( 2, store1.inventory.length )
+  }),
+  it("sell record", function(){
+    var store1 = new Store( "Stone Grooves", "Aberdeen", [], 500 );
+    var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99);
+    var record2 = new Record ( "Stone Roses", "Stone Roses", 5.99);
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.sellRecord(record1);
+    assert.equal( 504.99, store1.cash )
   })  
 })
--- week_6/weekend/record_store/store.js
+++ week_6/weekend/record_store/store.js
@@ -9,6 +9,25 @@ Store.prototype = {
   addRecord: function(record){
     this.inventory.push(record);
     return this.inventory;
+  },
+  listInventory: function(){
+    for(record of this.inventory){
+      console.log("Title : " + record.title);
+      console.log("Artist : " + record.artist);
+      console.log("Price : £ " + record.price);
+      console.log(" ");
+
+    }
+  },
+  sellRecord: function(record){
+    for(sold of this.inventory){
+      if(record.title === sold.title){
+        var toSell = sold;
+      }
+    }
+    console.log(toSell);
+    this.cash += toSell.price;
+    console.log(this.cash)
   }
 }
 
