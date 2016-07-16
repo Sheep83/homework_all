15da7f9c00b107f62ee18ccb37818fca026bbbc5
Sheep83 <bmorrice83@googlemail.com>
2016-07-16 04:34:26 +0100

    records tested

---
+++ week_6/weekend/record_store/specs/record_spec.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var Record = require ( '../record' );
+
+describe('Record', function(){
+
+  it("has an artist", function(){
+    var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99)
+    assert.equal( "Metallica", record1.artist )
+  }),
+  it("has a title", function(){
+      var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99)
+      assert.equal( "Ride the Lightning", record1.title )
+    }),
+  it("has a price", function(){
+      var record1 = new Record ( "Metallica", "Ride the Lightning", 4.99)
+      assert.equal( 4.99, record1.price )
+    })
+})
+    
