310626be06d78949c6c8c59b19888a8ef6013a0c
Sheep83 <bmorrice83@googlemail.com>
2016-07-18 18:36:15 +0100

    initial commit

---
+++ week_7/day_1/quotes_start/public/app.js
@@ -0,0 +1,73 @@
+window.onload = main;
+
+var Quote = function(text, author){
+  this.text = text;
+  this.author = author;
+}
+
+function main(){
+  var btn = document.getElementById('add-button');
+  btn.onclick = handleClick; 
+  // var form = document.getElementById( 'quote-form' );
+  // form.onsubmit = function( event ){
+  //   event.preventDefault();
+  //   handleClick();
+  var quotes = [];
+  quote1 = new Quote ('quote text 1', 'brian');
+  quote2 = new Quote ('quote text 2', 'jay');
+  quote3 = new Quote ('quote text 3', 'rick');
+  quote4 = new Quote ('quote text 4', 'keith');
+
+  quotes.push(quote1);
+  quotes.push(quote2);
+  quotes.push(quote3);
+  quotes.push(quote4);
+  addToPage(quotes);
+}
+
+
+function addToPage(quotes){
+  var ulist = document.getElementById( 'quote-list' );
+
+  for (quote of quotes){
+    var text = quote.text;
+    var author = quote.author;
+
+    var li = document.createElement( 'li' );
+    li.innerText = text;
+
+    var cite = document.createElement( 'cite' );
+    cite.innerText = author;
+    li.appendChild(cite);
+
+    ulist.appendChild(li);
+  }
+}
+
+function handleClick(){
+  var txtInput = document.getElementById('quote-text-input');
+  var autInput = document.getElementById('author-text-input');
+  appendQuote(txtInput.value, autInput.value);
+  console.log(txtInput.value);
+  // input.value = '';
+}
+
+function appendQuote( text, author ){
+  var ul = document.getElementById('quote-list');
+  var li = document.createElement( 'li' );
+  li.innerText = text;
+  var cite = document.createElement( 'cite' );
+  cite.innerText = author;
+  li.appendChild( cite );
+  ul.appendChild( li );
+}
+
+
+
+
+
+
+
+
+
+
