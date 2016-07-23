1a6e905e73f4ba61d928d1933370eace30e7fc36
Sheep83 <bmorrice83@googlemail.com>
2016-07-23 09:01:51 +0100

    can view photos on page

--- week_7/weekend/nasa/public/app.js
+++ week_7/weekend/nasa/public/app.js
@@ -1,27 +1,32 @@
 
 window.onload = function () {
-    var url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy'
+    // var url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy'
+    var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy'
+
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function () {
         if (request.status === 200) {
             var jsonString = request.responseText;
             var data = JSON.parse(jsonString);
+            console.log(data);
             main(data);
+            
         }
     }
-    button = document.createElement( 'button' );
-    button.addEventListener('click', function(){
-        console.log('JPL redirect...')
-    })
+    // button = document.createElement( 'button' );
+    // button.addEventListener('click', function(){
+    //     console.log('JPL redirect...')
+    // })
     request.send();
 };
 
 var main = function (data) {
     populateSelect(data);
-    var cached = localStorage.getItem("selectedItem");
-    var selected = data.near_earth_objects[0];
+    // updateDisplay(data);
 
+    var cached = localStorage.getItem("selectedImg");
+    var selected = data.photos[0];
     if(cached){
         selected = JSON.parse(cached);
         document.querySelector('#info').selectedIndex = selected.index;
@@ -32,27 +37,55 @@ var main = function (data) {
 
 var updateDisplay = function (data) {
     var stats = document.querySelectorAll('#info p');
-    var url = data.nasa_jpl_url
-    stats[0].innerHTML = "<b>Object Name : </b>" + data.name;
-    stats[1].innerHTML = "<b>Object Name : </b>" + "<button type='button'><a href = " + url + ">JPL</a></button>";
-    stats[2].innerHTML = "<b>Threat to Earth : </b>" + data.is_potentially_hazardous_asteroid; 
+    console.log(data);
+    var url = data.img_src;
+    // document.getElementById("img-preview").src = url;
+    // console.log(data.img_src);
+    stats[0].innerHTML = "<b>Rover Name : </b>" + data.rover.name;
+    stats[1].innerHTML = "<b>Camera : </b>" + data.camera.full_name;
+    showImage(data.img_src, 400, 300, data.img_src);
+}
+function showImage(src, width, height, alt) {
+    var imgDiv = document.getElementById('img');
+    // console.log(imgDiv.firstChild);
+    if (imgDiv.firstChild){
+        // console.log(imgDiv.firstChild);
+        imgDiv.removeChild(imgDiv.firstChild);
+        var img = document.createElement("img");
+        img.src = src;
+        img.width = 400;
+        img.height = 300;
+        img.alt = src;
+        imgDiv.appendChild(img);
+    }else{var img = document.createElement("img");
+        img.src = src;
+        img.width = 400;
+        img.height = 300;
+        img.alt = src;
+        imgDiv.appendChild(img);
+    }
+    
 }
 
 var populateSelect = function (data) {
+    // var curiosityRover = [];
+    // var spiritRover = [];
+    // console.log(data);
     var dropDown = document.querySelector('#dropDown');
-    data.near_earth_objects.forEach(function (item, index) {
+    data.photos.forEach(function (item, index) {
         item.index = index;
         var option = document.createElement("option");
         option.value = index.toString();
-        option.text = item.name;
+        option.text = item.rover.name;
         dropDown.appendChild(option);
     });
     dropDown.style.display = 'block';
     dropDown.addEventListener('change', function (e) {
         var index = this.value;
-        var neo = data.near_earth_objects[index];
-        updateDisplay(neo);
-        localStorage.setItem("selectedItem",JSON.stringify(neo));
+        var img = data.photos[index];
+        // console.log(img);
+        updateDisplay(img);
+        localStorage.setItem("selectedImg",JSON.stringify(img));
     });
 }
 
