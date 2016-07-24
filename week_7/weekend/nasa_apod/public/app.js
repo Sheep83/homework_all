325fa0cb645c023a57ef4f3d33790c6b5416a724
Sheep83 <bmorrice83@googlemail.com>
2016-07-24 15:04:30 +0100

    refactored

--- week_7/weekend/nasa_apod/public/app.js
+++ week_7/weekend/nasa_apod/public/app.js
@@ -1,59 +1,26 @@
 window.onload = function () {
-
-    var calendar = document.getElementById('inputDate')
-    // console.log(calendar);
-    calendar.onchange = function(e, request){
-        selected = e.target.value;
-        url = 'https://api.nasa.gov/planetary/apod?&date=' + selected + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
-        console.log(url);
-        var request = new XMLHttpRequest();
-        request.open("GET", url);
-        request.onload = function () {
-            if (request.status === 200) {
-                var jsonString = request.responseText;
-                var data = JSON.parse(jsonString);
-                // console.log(data);
-                apodDisplay(data);
-                
-            }
-        }
-        request.send();
-    }
+    var url = 'https://api.nasa.gov/planetary/apod?&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+    var calendar = document.getElementById('inputDate');
     var button = document.getElementById('random');
-    button.onclick = randomDate();
-    console.log(randDate);
-    var url = 'https://api.nasa.gov/planetary/apod?date=' + randDate + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+    calendar.onchange = getByDate;
+    button.onclick = randomDate;
+    console.log(url);
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function () {
         if (request.status === 200) {
             var jsonString = request.responseText;
             var data = JSON.parse(jsonString);
-            console.log(data);   
         }
         main(data)
     }
     request.send();
 }
-    var url = 'https://api.nasa.gov/planetary/apod?&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
-    var request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.onload = function () {
-        if (request.status === 200) {
-            var jsonString = request.responseText;
-            var data = JSON.parse(jsonString);
-            // console.log(data);   
-        }
-        main(data)
-    }
-    request.send();
 
 
 var apodDisplay = function(data){
     var stats = document.querySelectorAll('#info p');
     var title = document.getElementById('subheading');
-    // console.log(title);
-    // console.log(data);
     title.innerHTML = "<b><center>" + data.title + "</center></b>"
     stats[0].innerHTML = "<b>Description : </b>" + data.explanation;
         stats[1].innerHTML = "<b>Copyright : </b>" + data.copyright;
@@ -63,11 +30,10 @@ var apodDisplay = function(data){
     }
 var showImage = function(src, width, height, alt) {
         var imgDiv = document.getElementById('img');
-        console.log(img);
         if (imgDiv.firstChild){
-            // console.log(imgDiv.firstChild);
             imgDiv.removeChild(imgDiv.firstChild);
             var img = document.createElement("img");
+            console.log(img);
             img.src = src;
             img.width = 400;
             img.height = 300;
@@ -85,7 +51,6 @@ var showImage = function(src, width, height, alt) {
 
 var main = function(data){
     apodDisplay(data);
-    // console.log(data);
 }
 
 var randomDate = function(){
@@ -95,7 +60,36 @@ var randomDate = function(){
   randYear = Math.floor(Math.random() * (2015 - 1996) + 1996);
   array.push(randYear, randMonth, randDay);
   randDate = array.join('-');
-  return randDate;
+  console.log(randDate);
+
+  url = 'https://api.nasa.gov/planetary/apod?date=' + randDate + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = function () {
+      if (request.status === 200) {
+          var jsonString = request.responseText;
+          var data = JSON.parse(jsonString);
+      }
+      main(data)
+  }
+  request.send();
+}
+
+var getByDate = function(event){
+    console.log(event);
+    selected = event.target.value;
+    url = 'https://api.nasa.gov/planetary/apod?&date=' + selected + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+    console.log(url);
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function () {
+        if (request.status === 200) {
+            var jsonString = request.responseText;
+            var data = JSON.parse(jsonString);
+        }
+        main(data)
+    }
+    request.send();
 }
 
 
