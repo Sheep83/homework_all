bfb3b5de09503721c9f08ae4b72f8c30f241fcdb
Sheep83 <bmorrice83@googlemail.com>
2016-07-24 14:19:42 +0100

    random working

--- week_7/weekend/nasa_apod/public/app.js
+++ week_7/weekend/nasa_apod/public/app.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 
     var calendar = document.getElementById('inputDate')
-    console.log(calendar);
+    // console.log(calendar);
     calendar.onchange = function(e, request){
         selected = e.target.value;
         url = 'https://api.nasa.gov/planetary/apod?&date=' + selected + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
@@ -12,14 +12,17 @@ window.onload = function () {
             if (request.status === 200) {
                 var jsonString = request.responseText;
                 var data = JSON.parse(jsonString);
-                console.log(data);
+                // console.log(data);
                 apodDisplay(data);
                 
             }
         }
         request.send();
     }
-    var url = 'https://api.nasa.gov/planetary/apod?&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+    var button = document.getElementById('random');
+    button.onclick = randomDate();
+    console.log(randDate);
+    var url = 'https://api.nasa.gov/planetary/apod?date=' + randDate + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function () {
@@ -32,12 +35,25 @@ window.onload = function () {
     }
     request.send();
 }
+    var url = 'https://api.nasa.gov/planetary/apod?&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function () {
+        if (request.status === 200) {
+            var jsonString = request.responseText;
+            var data = JSON.parse(jsonString);
+            // console.log(data);   
+        }
+        main(data)
+    }
+    request.send();
+
 
 var apodDisplay = function(data){
     var stats = document.querySelectorAll('#info p');
     var title = document.getElementById('subheading');
-    console.log(title);
-    console.log(data);
+    // console.log(title);
+    // console.log(data);
     title.innerHTML = "<b><center>" + data.title + "</center></b>"
     stats[0].innerHTML = "<b>Description : </b>" + data.explanation;
         stats[1].innerHTML = "<b>Copyright : </b>" + data.copyright;
@@ -47,7 +63,7 @@ var apodDisplay = function(data){
     }
 var showImage = function(src, width, height, alt) {
         var imgDiv = document.getElementById('img');
-        console.log(imgDiv);
+        console.log(img);
         if (imgDiv.firstChild){
             // console.log(imgDiv.firstChild);
             imgDiv.removeChild(imgDiv.firstChild);
@@ -69,7 +85,7 @@ var showImage = function(src, width, height, alt) {
 
 var main = function(data){
     apodDisplay(data);
-    console.log(data);
+    // console.log(data);
 }
 
 var randomDate = function(){
