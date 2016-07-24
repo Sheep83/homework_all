c8286eb9e30768f843acc1570db721393fd2029b
Sheep83 <bmorrice83@googlemail.com>
2016-07-24 16:52:16 +0100

    MVP

--- week_7/weekend/nasa_apod/public/app.js
+++ week_7/weekend/nasa_apod/public/app.js
@@ -3,8 +3,10 @@ window.onload = function () {
     var url = 'https://api.nasa.gov/planetary/apod?&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
     var calendar = document.getElementById('inputDate');
     var button = document.getElementById('random');
+    var historyDropDown = document.getElementById('history');
     calendar.onchange = getByDate;
     button.onclick = randomDate;
+    historyDropDown.onchange = getFromHistory;
     console.log(url);
     var request = new XMLHttpRequest();
     request.open("GET", url);
@@ -32,8 +34,6 @@ var apodDisplay = function(data){
 var showImage = function(src, width, height, alt) {
     var imgDiv = document.getElementById('img');
     if (imgDiv.firstChild){
-        console.log(imgDiv.firstChild);
-
         imgDiv.removeChild(imgDiv.firstChild);
         var img = document.createElement("img");
         img.src = src;
@@ -53,11 +53,11 @@ var showImage = function(src, width, height, alt) {
 
 var main = function(data){
     apodDisplay(data);
+    populateHistory(visited);
 }
 
 var randomDate = function(){
   array = [];
-  // console.log(history);
   randDay = Math.floor((Math.random() * 28) + 1);
   randMonth = Math.floor((Math.random() * 12) + 1);
   randYear = Math.floor(Math.random() * (2015 - 1996) + 1996);
@@ -71,9 +71,10 @@ var randomDate = function(){
       if (request.status === 200) {
           var jsonString = request.responseText;
           var data = JSON.parse(jsonString);
+          visited.push(data);
+          console.log(visited);
       }
-      visited.push(data);
-      console.log(visited);
+      
       main(data);
   }
   request.send();
@@ -83,19 +84,41 @@ var getByDate = function(event){
     console.log(event);
     selected = event.target.value;
     url = 'https://api.nasa.gov/planetary/apod?&date=' + selected + '&api_key=T1AUnT68vq8FeqlfaGROtZl5h6mk9iMoz9Z7MKNy';
-    console.log(url);
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function () {
         if (request.status === 200) {
             var jsonString = request.responseText;
             var data = JSON.parse(jsonString);
-        }
-        visited.push(data);
-        console.log(visited);
+            visited.push(data);
+            console.log(visited);    
+    }
         main(data);
     }
     request.send();
 }
 
+var populateHistory = function(visitedArray){
+    console.log(visitedArray);
+    var historyDropDown = document.querySelector('#history');
+    historyDropDown.innerHTML = ""
+    visitedArray.forEach(function (item, index) {
+        item.index = index;
+        var option = document.createElement("option");
+        option.value = index.toString();
+        option.text = item.date;
+        historyDropDown.appendChild(option);
+    });
+    historyDropDown.style.display = 'block';
+    
+}
+
+var getFromHistory = function(event){
+    console.log(event);
+    var index = this.value;
+    var img = visited[index];
+    console.log(index);    
+    apodDisplay(img);
+    // localStorage.setItem("selectedImg",JSON.stringify(img));
+}
 
