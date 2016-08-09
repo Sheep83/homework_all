385e9c4c9f7df31b67ab6dabb73bb35dfa7d9a5f
Sheep83 <bmorrice83@googlemail.com>
2016-08-09 21:16:31 +0100

    java intro

---
+++ week_10/day_2/Cat.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Cat{
+  private String name;
+  private ArrayList<Edible> belly;
+
+  public Cat(String name){
+    this.name = name;
+    this.belly = new ArrayList<Edible>();
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void eat(Edible food){
+    belly.add(food);
+  }
+
+  public int foodEaten(){
+    return belly.size();
+
+  }
+
+  public String goMental(){
+    return "Wheeeeeeee!";
+  }
+
+  public void spew(){
+    belly.clear();
+  }
+
+  ////java array.pop hack
+  public void miniSpew(){
+    belly.remove(belly.size()-1);
+  }
+
+  public String snort(Catnip catnip){
+    return this.goMental();
+  }
+
+}
---
+++ week_10/day_2/CatTest.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CatTest{
+  Cat cat;
+  CatFood catfood;
+  Fish fish;
+  Catnip catnip;
+
+
+
+  @Before
+  public void before() {
+    cat = new Cat("Theo");
+    catfood = new CatFood();
+    fish = new Fish();
+    catnip = new Catnip();
+
+  }
+
+  @Test
+  public void hasAName(){
+    assertEquals("Theo", cat.getName());
+  }
+
+  @Test
+  public void canEatCatFood(){
+    cat.eat(catfood);
+    assertEquals(1, cat.foodEaten());
+  }
+
+  @Test
+  public void canEatFish(){
+    cat.eat(fish);
+    assertEquals(1, cat.foodEaten());
+  }
+
+  @Test
+  public void canGoMental(){
+    assertEquals("Wheeeeeeee!", cat.goMental());
+  }
+
+  @Test
+  public void spewEmptiesBelly(){
+    cat.eat(fish);
+    cat.eat(catfood);
+    cat.spew();
+    assertEquals(0, cat.foodEaten());
+  }
+
+  @Test
+  public void canThrowUpOnlyLastItemEaten(){
+    cat.eat(fish);
+    cat.eat(catfood);
+    cat.miniSpew();
+    assertEquals(1, cat.foodEaten());
+  }
+
+  @Test
+  public void canTakeCatnip(){
+    assertEquals("Wheeeeeeee!", cat.snort(catnip));
+  }
+
+}
+
