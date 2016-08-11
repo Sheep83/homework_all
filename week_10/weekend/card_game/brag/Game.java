2e2d628ba7a6861286a52206f619efc626d02d8b
Sheep83 <bmorrice83@googlemail.com>
2016-08-11 15:57:02 +0100

    cards and players working

--- week_10/weekend/card_game/brag/Game.java
+++ week_10/weekend/card_game/brag/Game.java
@@ -3,21 +3,35 @@ import java.util.*;
 
 public class Game {
   public ArrayList<Player> players;
+  private Player player1;
+  private Player player2;
 
   public Game(){
 
   }
 
+  public void getPlayer1Name(){
+    System.out.println(player1.name);
+  }
+
+  public void getPlayer2Name(){
+    System.out.println(player2.name);
+  }
+
   public void createPlayers(){
-    Player player1 = new Player("Brian");
-    Player player2 = new Player("Josh");
+    player1 = new Player("Brian");
+    player2 = new Player("Josh");
 
   }
 
-  public static void main(String[] args){
-          Deck deck = new Deck();
+  // public static void main(String[] args){
+          
+  //         Deck deck = new Deck();
 
-          //print out the deck.
-          System.out.println(deck.getDeck());
-      }
+
+  //         //print out the deck.
+  //         System.out.println(deck.getDeck());
+  //         game.getPlayer();
+  //         System.out.println(player1.name);
+      // }
 }
